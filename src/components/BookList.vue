306811328a389d36b8c9cd4ce1e7d06930b579bb
dpnetca <git@dpnet.ca>
2020-02-29 13:58:12 -0600

    Added BookForm

--- src/components/BookList.vue
+++ src/components/BookList.vue
@@ -6,15 +6,18 @@
         <book-item :book="book"></book-item>
       </div>
     </ul>
+    <book-form @addBook="appendBook"></book-form>
   </div>
 </template>
 <script>
 import BookItem from "./BookItem";
+import BookForm from "./BookForm";
 
 export default {
   name: "BookList",
   components: {
-    BookItem
+    BookItem,
+    BookForm
   },
   data() {
     return {
@@ -25,6 +28,11 @@ export default {
         { title: "Amusing Ourselves to Death", author: "Neil Postman" }
       ]
     };
+  },
+  methods: {
+    appendBook(bookTitle, bookAuthor) {
+      this.books.push({ title: bookTitle, author: bookAuthor });
+    }
   }
 };
 </script>
